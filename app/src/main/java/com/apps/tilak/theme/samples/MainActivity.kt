89f9ec4bd2d85c82acf1bbd4960688070b69272d
tilak <tilakas13@gmail.com>
2021-01-09 19:00:26 +0530

    basic dark theme setting

--- app/src/main/java/com/apps/tilak/theme/samples/MainActivity.kt
+++ app/src/main/java/com/apps/tilak/theme/samples/MainActivity.kt
@@ -2,10 +2,28 @@ package com.apps.tilak.theme.samples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
+import com.apps.tilak.theme.samples.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val  TAG = "MainActivity"
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnDarkTheme.setOnClickListener {
+            Log.d(TAG, "dark theme button click ")
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        binding.btnLightTheme.setOnClickListener {
+            Log.d(TAG, "Light theme button click ")
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
     }
 }
